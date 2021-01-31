98fd9d61ba08a82609f11c2b6272a6064167fa9a
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:25:55 +0330

    Add routes in routes.js

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,8 +1,31 @@
-import Home from "./components/Home";
+import Index from "./components/Products/Index";
+import ProductShow from "./components/Products/Show";
+
+import Checkout from "./components/Order/Checkout";
+import Summary from "./components/Order/Summary";
 
 export const routes = [
     {
-        path: '/',
-        component: Home,
+        path: '/'   ,
+        name: 'products.index',
+        component: Index
     },
+
+    {
+        path: '/products/:slug',
+        name: 'products.show',
+        component: ProductShow
+    },
+
+    {
+        path: '/checkout',
+        name: 'order.checkout',
+        component: Checkout
+    },
+
+    {
+        path: '/summary',
+        name: 'order.summary',
+        component: Summary
+    }
 ]
