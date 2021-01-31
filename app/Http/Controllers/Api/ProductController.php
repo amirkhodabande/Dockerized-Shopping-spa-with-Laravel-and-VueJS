949fbfe8fd48a7b82867872f08c35271e24c9877
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:22:46 +0330

    Add ProductController.php

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        return Product::with('categories:id,name')
+            ->get();
+    }
+
+}
