9845e3a33d513077df5f2108682fcddc65dd67f2
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:27:25 +0330

    Add app.blade.php as main blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shop-Laravel_Vue</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+<body class="antialiased">
+
+<div class="container mx-auto">
+    <div id="app">
+        <main-app></main-app>
+    </div>
+</div>
+
+<!-- Scripts-->
+<script src="{{ mix('js/app.js') }}"></script>
+
+</body>
+</html>
