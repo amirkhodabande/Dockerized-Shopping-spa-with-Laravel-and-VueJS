d34109fa8b013daaed402879166852b123303cc2
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:22:00 +0330

    Edit DatabaseSeeder.php to create some test products with categories and connect random categories to random products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $products = Product::factory(20)->create();
+        $categories = Category::factory(5)->create();
+
+        $products->each(function ($product) use ($categories) {
+            $product->categories()->attach(
+                $categories->random(2)->pluck('id')->toArray()
+            );
+        });
+
     }
 }
