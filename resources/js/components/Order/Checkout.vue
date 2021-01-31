55d590112c34288cfb0908971fd1cf3fbc79ea46
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:26:55 +0330

    Add Order Checkout.vue and Summary.vue

---
+++ resources/js/components/Order/Checkout.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Checkout</h1>
+</template>
+
+<script>
+export default {
+    name: "Checkout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Order/Summary.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Summary</h1>
+</template>
+
+<script>
+export default {
+    name: "Summary"
+}
+</script>
+
+<style scoped>
+
+</style>
