fb9a31227b1b87911ecc1e67b2c59033c1941c74
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 13:16:10 +0330

    Intitial commit

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,13 @@
+<template>
+ <div><router-view></router-view></div>
+</template>
+
+<script>
+export default {
+    name: "MainApp"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/routes.js
@@ -0,0 +1,8 @@
+import Home from "./components/Home";
+
+export const routes = [
+    {
+        path: '/',
+        component: Home,
+    },
+]
