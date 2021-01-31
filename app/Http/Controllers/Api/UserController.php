9e4b18be2c2bb88f53105656a810b49f2f7173f4
Amir Khodabande <amirdp8102@gmail.com>
2021-01-31 16:22:34 +0330

    Add UserController.php

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function purchase(Request $request)
+    {
+
+    }
+}
