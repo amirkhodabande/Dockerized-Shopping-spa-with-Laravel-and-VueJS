766a5307d97c4b4f998981afb9f136fd54c95d68
Amir Khodabande <amirdp8102@gmail.com>
2021-02-02 16:17:14 +0330

    Show all selected products in Checkout.vue and get user information for pay

--- resources/js/components/Order/Checkout.vue
+++ resources/js/components/Order/Checkout.vue
@@ -1,10 +1,218 @@
 <template>
-    <h1>Checkout</h1>
+    <div class="w-full">
+        <div class="lg:w-1/2 w-full mx-auto mt-8 overflow-auto">
+            <table class="table-auto w-full text-left whitespace-nowrap">
+                <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 rounded-tl rounded-bl">
+                        Item
+                    </th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 ">
+                        Quantity
+                    </th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 ">
+                        Price
+                    </th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 rounded-tr rounded-br">
+                        Actions
+                    </th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr v-for="(item, index) in cart" :key="item.id">
+                    <td class="p-4" v-text="item.name"></td>
+                    <td class="p-4" v-text="item.quantity"></td>
+                    <td class="p-4" v-text="cartLineTotal(item)"></td>
+                    <td class="p-4">
+                        <button
+                            class="flex ml-auto text-sm text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
+                            @click="$store.commit('removeFromCart', index)"
+                        >Remove
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="p-4 font-bold">Total Amount</td>
+                    <td class="p-4 font-bold" v-text="cartQuantity"></td>
+                    <td class="p-4 font-bold" v-text="cartTotal"></td>
+                    <td class="w-10 "></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="lg:w-2/3 w-full mx-auto mt-8">
+            <div class="flex flex-wrap -mx-2 mt-8">
+
+                <!-- Start getting user information-->
+                <div class="p-2 w-1/3">
+                    <div class="relative">
+                        <label for="first_name" class="leading-7 text-sm text-gray-600">First Name</label>
+                        <input
+                            type="text"
+                            id="first_name"
+                            name="first_name"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.first_name"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <div class="p-2 w-1/3">
+                    <div class="relative">
+                        <label for="last_name" class="leading-7 text-sm text-gray-600">Last Name</label>
+                        <input
+                            type="text"
+                            id="last_name"
+                            name="last_name"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.last_name"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <div class="p-2 w-1/3">
+                    <div class="relative">
+                        <label for="email" class="leading-7 text-sm text-gray-600">Email Address</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.email"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-2 mt-4">
+                <div class="p-2 w-1/3">
+                    <div class="relative">
+                        <label for="address" class="leading-7 text-sm text-gray-600">Street Address</label>
+                        <input
+                            type="text"
+                            id="address"
+                            name="address"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.address"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <div class="p-2 w-1/3">
+                    <div class="relative">
+                        <label for="city" class="leading-7 text-sm text-gray-600">City</label>
+                        <input
+                            type="text"
+                            id="city"
+                            name="city"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.city"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <div class="p-2 w-1/6">
+                    <div class="relative">
+                        <label for="state" class="leading-7 text-sm text-gray-600">State</label>
+                        <input
+                            type="email"
+                            id="state"
+                            name="state"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.state"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <div class="p-2 w-1/6">
+                    <div class="relative">
+                        <label for="zip_code" class="leading-7 text-sm text-gray-600">Zip Code</label>
+                        <input
+                            type="email"
+                            id="zip_code"
+                            name="zip_code"
+                            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            v-model="customer.zip_code"
+                            :disabled="paymentProcessing"
+                        >
+                    </div>
+                </div>
+                <!--End-->
+            </div>
+
+            <div class="flex flex-wrap -mx-2 mt-4">
+                <div class="p-2 w-full">
+                    <div class="relative">
+                        <label for="card_element" class="leading-7 text-sm text-gray-600">Credit Card
+                            Information</label>
+                        <div id="card-element"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-2 w-full">
+                <button
+                    class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                    @click="processPayment"
+                    :disabled="paymentProcessing"
+                    v-text="paymentProcessing ? 'Processing' : 'Pay Now'"
+                ></button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+
 export default {
-    name: "Checkout"
+    name: "Checkout",
+
+    data() {
+        return {
+
+            customer: {
+                first_name: '',
+                last_name: '',
+                email: '',
+                address: '',
+                city: '',
+                state: '',
+                zip_code: '',
+            },
+            paymentProcessing: false,
+        }
+    },
+
+    computed: {
+        cart() {
+            return this.$store.state.cart;
+        },
+
+        cartQuantity() {
+            return this.$store.state.cart.reduce((acc, item) => acc + item.quantity, 0);
+        },
+
+        cartTotal() {
+            let price = this.cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+            price = (price / 100);
+
+            return price.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+        }
+    },
+    methods: {
+        cartLineTotal(item) {
+            let price = (item.price * item.quantity);
+            price = (price / 100);
+
+            return price.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+        },
+
+        processPayment() {
+
+        }
+    }
 }
 </script>
 
