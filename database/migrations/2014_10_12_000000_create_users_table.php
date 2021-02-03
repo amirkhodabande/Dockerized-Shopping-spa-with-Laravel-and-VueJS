a29fabeee1229e9f99457e7ac19dc3b97aec4b49
Amir Khodabande <amirdp8102@gmail.com>
2021-02-03 08:53:53 +0330

    Edit users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip_code');
             $table->rememberToken();
             $table->timestamps();
         });
