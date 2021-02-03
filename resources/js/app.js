d66e19498e194c14b8ee1602bd44e4c419190ded
Amir Khodabande <amirdp8102@gmail.com>
2021-02-03 09:13:55 +0330

    Import and config v-toaster in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,15 @@ import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import {routes} from './routes'
 import StoreData from './store'
+import Toaster from 'v-toaster'
+import 'v-toaster/dist/v-toaster.css'
 
 Vue.use(VueRouter);
 
 Vue.use(Vuex);
 const store = new Vuex.Store(StoreData)
+
+Vue.use(Toaster, {timeout: 5000})
 
 const router = new VueRouter({
     routes,
