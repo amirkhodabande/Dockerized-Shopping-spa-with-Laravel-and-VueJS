37fe35e665865bbef22dfe11df81a034fa61e587
Amir Khodabande <amirdp8102@gmail.com>
2021-02-03 17:43:45 +0330

    Add authentication routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'auth'], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
 
 Route::get('/products', [ProductController::class, 'index']);
 
