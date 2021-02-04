b024cbb92aecffe1e593644f2acc0481acb346eb
Amir Khodabande <amirdp8102@gmail.com>
2021-02-04 10:32:04 +0330

    Add helpers/general.js

---
+++ resources/js/helpers/general.js
@@ -0,0 +1,38 @@
+export function initialize(store, router) {
+    //Auth check
+    router.beforeEach((to, from, next) => {
+        const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+        const currentUser = store.state.currentUser;
+
+
+        if (requiresAuth && !currentUser) {
+            next('/login');
+        } else if (to.path === '/login' || to.path === '/register') {
+            if (currentUser)
+                next('/');
+            next();
+        } else {
+            next();
+        }
+    });
+
+    //Errors
+    axios.interceptors.response.use(null, (error) => {
+        if (error.response.status === 401) {
+            store.commit('logout');
+            router.push('/login');
+        }
+
+        return Promise.reject(error);
+    });
+
+    //Add default header to all axios requests
+    if (store.getters.currentUser) {
+        setAuthorization(store.getters.currentUser.token);
+    }
+
+}
+
+export function setAuthorization(token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+}
