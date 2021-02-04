c551270720caafb707dfba66a25a5118bc1e4b4b
Amir Khodabande <amirdp8102@gmail.com>
2021-02-04 10:32:33 +0330

    Add MainApp.vue and use Header.vue

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="wrapper">
+        <Header/>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+import Header from "./Header";
+export default {
+    name: "main-app",
+
+    components: {Header},
+}
+</script>
+
+<style scoped>
+
+</style>
